0ad40c003707fc11cab9afb88aa657542c6083e4
tiago <kage3f@gmail.com>
2024-02-27 15:23:06 -0300

    test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Login</title>
+    
 </head>
 <body>
     <div class="container">
